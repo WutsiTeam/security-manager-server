7d506b466083e63eeb920e4d15466e4f57d15cca
wutsi <herve.tchepannou@wutsi.com>
2022-11-05 16:29:46 -0400

    update dependencies

--- src/main/kotlin/com/wutsi/security/service/ApiKeyAuthenticatorImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.wutsi.security.service
-
-import com.wutsi.platform.core.security.spring.ApiKeyAuthenticator
-import org.springframework.stereotype.Service
-
-@Service
-class ApiKeyAuthenticatorImpl : ApiKeyAuthenticator {
-    override fun authenticate(apiKey: String): String = ""
-}
