dd8af7ae079e85ae495ebeddd233f925823066fe
wutsi <herve.tchepannou@wutsi.com>
2022-11-09 10:56:48 -0500

    udpdate schema

--- src/main/kotlin/com/wutsi/security/dto/CreatePasswordRequest.kt
+++ src/main/kotlin/com/wutsi/security/dto/CreatePasswordRequest.kt
@@ -1,6 +1,7 @@
 package com.wutsi.security.dto
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 import kotlin.Long
 import kotlin.String
 
@@ -9,5 +10,6 @@ public data class CreatePasswordRequest(
     public val `value`: String = "",
     public val accountId: Long = 0,
     @get:NotBlank
+    @get:Size(max = 30)
     public val username: String = ""
 )
